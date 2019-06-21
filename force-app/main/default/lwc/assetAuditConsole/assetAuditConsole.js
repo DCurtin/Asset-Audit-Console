a9037e4b1c5e4994f04759e05193c9182e456be1
Dalton Curtin <dcurtin@midland.local>
2019-06-21 16:30:19 -0400

    Wrote out starter interface that takes input from user and provides a execution button. Need to start the apex helper class for generating the report with the input. Will likely include a wrapper class as well.

--- force-app/main/default/lwc/assetAuditConsole/assetAuditConsole.js
+++ force-app/main/default/lwc/assetAuditConsole/assetAuditConsole.js
@@ -1,7 +1,40 @@
 import { LightningElement, track } from 'lwc';
 
 export default class AssetAuditConsole extends LightningElement {
-@track returnedRecordCount = 0;
+    @track returnedRecordCount;
+    @track endDateStatus;
+    @track filterPercentDifference=0.20;
 
+    startDate;
+    endDate;
+    
 
+    constructor()
+    {
+        super();
+        this.returnedRecordCount=0;
+    }
+
+    generateReport()
+    {
+        this.returnedRecordCount +=1;
+
+    }
+
+    updateStartDate(event)
+    {
+        this.startDate=event.detail.value;
+        
+    }
+
+    updateEndDate(event)
+    {
+        this.endDate=event.detail.value;
+        this.endDateStatus=event.detail.value;
+    }
+
+    updateFilterPercentDifference(event)
+    {
+        this.filterPercentDifference=event.detail.value;
+    }
 }

6886ddd74a9163fdd2e8bb752f39d7421924222c
Dalton Curtin <dcurtin@midland.local>
2019-06-24 14:17:23 -0400

    Created conceptual UI and started staging some of the imperative call and wrapper class. The imperative call is just querying a random set of transactions in order to generate test data for testing aspects of the ui design.

--- force-app/main/default/lwc/assetAuditConsole/assetAuditConsole.js
+++ force-app/main/default/lwc/assetAuditConsole/assetAuditConsole.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-console */
 import { LightningElement, track } from 'lwc';
+import generateAssetAuditReport from '@salesforce/apex/AssetAuditConsole.generateAssetAuditReport';
+
+const columns = [
+    { label: 'Name', fieldName: 'Name'},
+    { label: 'Amount (Cash Total)', fieldName: 'Amount_Cash_Total__c', type: 'currency'},
+    { label: 'Date Complete', fieldName: 'Date_Complete__c', type: 'date'},
+    { label: 'Type', fieldName: 'Transaction_Type__c'}
+]
 
 export default class AssetAuditConsole extends LightningElement {
     @track returnedRecordCount;
     @track endDateStatus;
     @track filterPercentDifference=0.20;
+    @track fullSalesReport;
+    @track columns = columns;
 
     startDate;
     endDate;
@@ -17,8 +28,14 @@ export default class AssetAuditConsole extends LightningElement {
 
     generateReport()
     {
-        this.returnedRecordCount +=1;
+        //this.returnedRecordCount +=1;
+        generateAssetAuditReport({startDate: this.startDate, endDate: this.endDate}).then(function(result)
+        {
+            console.log(result[0].debitList);
+            this.returnedRecordCount=result.length;
+            this.fullSalesReport = result;
 
+        }.bind(this));
     }
 
     updateStartDate(event)
